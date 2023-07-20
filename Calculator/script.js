e76ec12c2f68d2de43fca1f53f6ab83d69b532a6
Vaishnav Ghangale <107029372+vaishnav196@users.noreply.github.com>
2023-07-20 11:15:24 +0530

    Update script.js

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,6 +1,130 @@
-let light=" styles/light.css";
-let dark="style/dark.css";
-const res=document.getElementById("res");
+let light = " styles/light.css";
+let dark = "style/dark.css";
+let res = document.querySelector(".result").value;
+// res=''
 
+let keys = document.querySelectorAll(".btn");
+// for( i=0; i <keys.length; i++){
+ 
+// document.querySelectorAll('.btn').addEventListener()
+    // console.log(keys[i].value);
+// }
+document.addEventListener('click',(keypress)=>{
+    // console.log("key is press");
+    n=keypress.target.value;
+    if(n=="1"){
+console.log("one is pressed");
+
+res=res+n;
+document.getElementById("display").value=res;
+    }
+    else if(n=="2"){
+        console.log("2 is pressed");
+        res=res+n;
+document.getElementById("display").value=res;
+    }
+    else if(n=="3"){
+        console.log("3 is pressed");
+        res=res+n;
+document.getElementById("display").value=res;
+    }
+     
+    else if(n=="4"){
+        console.log("4 is pressed");
+        res=res+n;
+document.getElementById("display").value=res;
+    }
+     
+    else if(n=="5"){
+        console.log("5 is pressed");
+        res=res+n;
+document.getElementById("display").value=res;
+    }
+     
+    else if(n=="6"){
+        console.log("6 is pressed");
+        res=res+n;
+document.getElementById("display").value=res;
+    }
+     
+    else if(n=="7"){
+        console.log("7 is pressed");
+        res=res+n;
+document.getElementById("display").value=res;
+    }
+     
+    else if(n=="8"){
+        console.log("8 is pressed");
+        res=res+n;
+document.getElementById("display").value=res;
+    }
+     
+    else if(n=="9"){
+        console.log("9 is pressed");
+        res=res+n;
+document.getElementById("display").value=res;
+    }
+     
+    else if(n=="0"){
+        console.log("0 is pressed");
+        res=res+n;
+document.getElementById("display").value=res;
+    }
+     
+    else if(n=="."){
+        console.log(". is pressed");
+        res=res+n;
+document.getElementById("display").value=res;
+    }
+     
+    else if(n=="*"){
+        console.log("* is pressed");
+        res=res+n;
+document.getElementById("display").value=res;
+    }
+    else if(n=="/"){
+        console.log("/ is pressed");
+        res=res+n;
+document.getElementById("display").value=res;
+    }
+    else if(n=="-"){
+        console.log("-  is pressed");
+        res=res+n;
+document.getElementById("display").value=res;
+    }
+    else if(n=="+"){
+        console.log("+ is pressed");
+        res=res+n;
+document.getElementById("display").value=res;
+    }
+
+    else{
+        
+    }
+     
+
+       })
+function keypress(){
+
+}
+function calculator(){
+
+
+
+
+    // Array.from.forEach((btn)=>{
+    //     btn.addEventListener('click',(e)=> {
+    //       console.log(e.target);
+    //       res =res + e.target;
+    //       document.querySelector(".result").value=res;
+    //     })
+    // })
+    
+}
+
+
+  
+
+    
 
 
