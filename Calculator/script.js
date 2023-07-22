d26af4b15a680b76977809dd14237d072f6bb8d9
Vaishnav Ghangale <107029372+vaishnav196@users.noreply.github.com>
2023-07-22 17:29:45 +0530

    Update script.js

--- Calculator/script.js
+++ Calculator/script.js
@@ -2,131 +2,35 @@ let light = " styles/light.css";
 let dark = "style/dark.css";
 let res = document.querySelector(".result").value;
 // res=''
-
 let keys = document.querySelectorAll(".btn");
+let theme=document.getElementById("moon").value;
 
 
-document.addEventListener('click',(keypress)=>{
-  
-    n=keypress.target.value;
-    if(n=="1"){
-console.log("one is pressed");
 
-res=res+n;
-document.getElementById("display").value=res;
-    }
-    else if(n=="2"){
-        console.log("2 is pressed");
-        res=res+n;
-document.getElementById("display").value=res;
-    }
-    else if(n=="3"){
-        console.log("3 is pressed");
-        res=res+n;
-document.getElementById("display").value=res;
-    }
-     
-    else if(n=="4"){
-        console.log("4 is pressed");
-        res=res+n;
-document.getElementById("display").value=res;
-    }
-     
-    else if(n=="5"){
-        console.log("5 is pressed");
-        res=res+n;
-document.getElementById("display").value=res;
-    }
-     
-    else if(n=="6"){
-        console.log("6 is pressed");
-        res=res+n;
-document.getElementById("display").value=res;
-    }
-     
-    else if(n=="7"){
-        console.log("7 is pressed");
-        res=res+n;
-document.getElementById("display").value=res;
-    }
-     
-    else if(n=="8"){
-        console.log("8 is pressed");
-        res=res+n;
-document.getElementById("display").value=res;
-    }
-     
-    else if(n=="9"){
-        console.log("9 is pressed");
-        res=res+n;
-document.getElementById("display").value=res;
-    }
-     
-    else if(n=="0"){
-        console.log("0 is pressed");
-        res=res+n;
-document.getElementById("display").value=res;
-    }
-     
-    else if(n=="."){
-        console.log(". is pressed");
-        res=res+n;
-document.getElementById("display").value=res;
-    }
-     
-    else if(n=="*"){
-        console.log("* is pressed");
-        res=res+n;
-document.getElementById("display").value=res;
-    }
-    else if(n=="/"){
-        console.log("/ is pressed");
-        res=res+n;
-document.getElementById("display").value=res;
-    }
-    else if(n=="-"){
-        console.log("-  is pressed");
-        res=res+n;
-document.getElementById("display").value=res;
-    }
-    else if(n=="+"){
-        console.log("+ is pressed");
-        res=res+n;
-document.getElementById("display").value=res;
-    }
 
-    else{
+     Array.from(keys).forEach((el) =>{
+      el.addEventListener('click',(i) =>{
+        num = i.target.value;
+        if(num=='C'){
+          res= res.slice(0,-1)
         
-    }
+          document.getElementById("display").value=res
+        }
      
-
-       })
-
-function calculator(){
-
-
-
-
-    
-}
-
-
-  
-
-    
-    //  let display =  document.getElementById("text");
-    //  display = "";
-    //  let keys = document.querySelectorAll(".button");
-    //  console.log(keys);
-    //  Array.from(keys).forEach((el) =>{
-    //   el.addEventListener('click',(i) =>{
-    //         console.log(i.target.value);
-    //         num = i.target.value;
-    //         display = display + num;
-    //         document.getElementById("text").value = display;
-
-    //   });
-    //  });
+        else if(num=='='){
+          res=eval(res)
+          document.getElementById("display").value=res;
+        }
+        
+        else{
+          num = i.target.value;
+          res = res + num;
+          document.getElementById("display").value = res;
+        }
+            
+      });
+     
+     });
 
 
 
